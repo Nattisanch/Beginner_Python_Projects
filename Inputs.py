a70d610cdfd50edca7b6cebd8c14af178b388613
Natalie <125685690+Nattisanch@users.noreply.github.com>
2024-07-13 21:01:45 -0400

    added if statement

--- Inputs.py
+++ Inputs.py
@@ -4,4 +4,7 @@ name = input('Name:')
  
 age = input('Enter Age:')
 
-print ('Hello I am',name,"and I'm", age,"Years old")
+if name == '' and age == '':
+    print ('Please insert your name and age!')
+else:   
+    print ('Hello I am',name,"and I'm", age,"Years old")
