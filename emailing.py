9a724f7b18dc193a40861e1b65d805627560536a
Natalie <125685690+Nattisanch@users.noreply.github.com>
2024-08-14 07:38:51 -0400

    Removed attachment

--- emailing.py
+++ emailing.py
@@ -2,14 +2,8 @@ import win32com.client as win32
 
 outlook = win32.Dispatch('outlook.application')
 user=input('Please enter a email to send to: ')
-mail = outlook.CreateItem(0)
-mail.To = user
-mail.Subject = 'My emailing script for python'
-mail.Body = 'Testing my automating skills with python'
-
-
-# To attach a file to the email (optional):
-#attachment  = "Path to the attachment"
-#mail.Attachments.Add(attachment)
-
-mail.Send()
+email = outlook.CreateItem(0)
+email.To = user
+email.Subject = 'My emailing script for python'
+email.Body = 'Testing my automating skills with python'
+email.Send()
